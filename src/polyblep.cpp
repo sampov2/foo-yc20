262f76806bd07979cb316c5f054bf609935f83f9
sampo.savolainen <sampo.savolainen@f5e34f95-6847-7746-d8f2-e330973f9afd>
2010-04-16 21:43:11 +0000

    updated copyright clauses. One was missing and others had some wrong years.

--- src/polyblep.cpp
+++ src/polyblep.cpp
@@ -1,3 +1,22 @@
+/*
+    Foo-YC20 UI (header)
+    Copyright (C) 2009  Sampo Savolainen <v2@iki.fi>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 #include <sys/types.h>
 #include <math.h>
 #include <stdio.h>
