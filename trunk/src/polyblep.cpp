05722a17c9e695e693edffca14deef8ec6ab5f6c
sampo.savolainen <sampo.savolainen@f5e34f95-6847-7746-d8f2-e330973f9afd>
2010-08-04 19:23:13 +0000

    Double/float conversion inefficiency. Fix by Stèphane Letz. Thanks!

--- trunk/src/polyblep.cpp
+++ trunk/src/polyblep.cpp
@@ -21,42 +21,47 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 inline float calculate_polyblep_valimaki_huovilainen(float t)
 {
 	if (t>0) {
-		return (t - (t*t)/2.0 - 0.5);
+		return (t - (t*t)/2.0f - 0.5f);
 	} else {
-		return ((t*t)/2.0 + t + 0.5);
+		return ((t*t)/2.0f + t + 0.5f);
 	}
 }
 
 #define polyblep_it(x) calculate_polyblep_valimaki_huovilainen((x))
 
 
-inline float saw_polyblep_fast(float ph, float ph_, float ph__, float q)
+//inline float saw_polyblep_fast(float ph, float ph_, float ph__, float q)
+float saw_polyblep_fast(float ph, float ph_, float ph__, float q)
 {
-	float mod = 0.0;
+	float mod = 0.0f;
 
 	// the sample before the discontinuity
 	if (ph_ > ph) {
-		mod = polyblep_it( (ph_ - 1.0) / q );
+		mod = polyblep_it( (ph_ - 1.0f) / q );
 	} else
 	// the sample after the discontinuity
 	if (ph__ > ph_) {
 		mod = polyblep_it( ph_ / q );
 	}
 
-	return mod * 2.0;
+	return mod * 2.0f;
 }
 
 inline float square_polyblep_fast(float ph, float ph_, float ph__, float q)
 {
-	float mod = 0.0;
+	float mod = 0.0f;
 
 	// ** start-of-phase discontinuity
 	// the sample before the discontinuity
 	if (ph_ > ph) {
-		mod = polyblep_it( (ph_ - 1.0) / q );
+		mod = polyblep_it( (ph_ - 1.0f) / q );
 	} else
 	// the sample after the discontinuity
 	if (ph__ > ph_) {
@@ -64,21 +69,22 @@ inline float square_polyblep_fast(float ph, float ph_, float ph__, float q)
 	} else
 	// ** mid-phase discontinuity
 	// the sample before the discontinuity
-	if (ph_ < 0.5 && 0.5 <= ph) {
-		mod = -polyblep_it( (ph_ - 0.5) / q );
+	if (ph_ < 0.5f && 0.5f <= ph) {
+		mod = -polyblep_it( (ph_ - 0.5f) / q );
 	} else
 	// the sample after the discontinuity
-	if (ph__ < 0.5 && 0.5 <= ph_) {
-		mod = -polyblep_it( (ph_ - 0.5) / q );
+	if (ph__ < 0.5f && 0.5f <= ph_) {
+		mod = -polyblep_it( (ph_ - 0.5f) / q );
 	}
 
-	return mod * 2.0;
+	return mod * 2.0f;
 }
 
-inline float square_polyblep_precalc(float ph, float ph_, float ph__, float precalc)
+//inline float square_polyblep_precalc(float ph, float ph_, float ph__, float precalc)
+float square_polyblep_precalc(float ph, float ph_, float ph__, float precalc)
 {
-	if (precalc == 0.0) {
-		return 0.0;
+	if (precalc == 0.0f) {
+		return 0.0f;
 	}
 
 	// ** start-of-phase discontinuity
@@ -86,16 +92,16 @@ inline float square_polyblep_precalc(float ph, float ph_, float ph__, float prec
 		return precalc;
 	} else
 	// ** mid-phase discontinuity
-	if (ph__ < 0.5 && 0.5 <= ph) {
+	if (ph__ < 0.5f && 0.5f <= ph) {
 		return -precalc;
 	}
-	return 0.0;
+	return 0.0f;
 }
 
 
 inline float square_wave(float f)
 {
-	f -= 0.5;
+	f -= 0.5f;
 
 	uint32_t *int_ptr = (uint32_t *)&f;
 	uint32_t i = *int_ptr;
@@ -106,10 +112,14 @@ inline float square_wave(float f)
 
 	f = (float)i;
 
-	f -= 0.5;
-	f *= 2.0;
+	f -= 0.5f;
+	f *= 2.0f;
 	
 	return f;
 }
 
 
+#ifdef __cplusplus
+}
+#endif
+				
