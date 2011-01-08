0f04aaca0f5054703205de816ee7643e0d9a9307
sampo.savolainen <sampo.savolainen@f5e34f95-6847-7746-d8f2-e330973f9afd>
2011-01-08 20:38:12 +0000

    Updated environment plus icon for the windows version

--- tools/win32/env.sh
+++ tools/win32/env.sh
@@ -1,3 +1,6 @@
+
+THIS_PATH=$(dirname $(readlink -f $0))
+
 export CC=i586-mingw32msvc-gcc
 export CXX=i586-mingw32msvc-g++
 export LD=i586-mingw32msvc-ld
@@ -9,9 +12,15 @@ export RANLIB=i586-mingw32msvc-ranlib
 export DLLTOOL=i586-mingw32msvc-dlltool
 export OBJDUMP=i586-mingw32msvc-objdump
 export RC=i586-mingw32msvc-windres
+export WINDRES=i586-mingw32msvc-windres
 
-export PATH=/home/v2/dev/mine/win32/fake-utils:$PATH
+export PATH=$THIS_PATH/fake-utils:$PATH
 
-export LDFLAGS_YC20=-mwindows
+export LDFLAGS_YC20="-mwindows "$THIS_PATH/gui.o
 export CFLAGS="-O3 -mtune=native -march=native -msse -mfpmath=sse -ffast-math"
+
+if [ ! -f $THIS_PATH/gui.o ]; then
+	echo compiling gui.rc
+	$WINDRES $THIS_PATH/gui.rc -o $THIS_PATH/gui.o
+fi
 
