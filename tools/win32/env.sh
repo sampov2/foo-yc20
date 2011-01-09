6c855d75ac1230bab7d090c3aa10d21bfbcdee1f
sampo.savolainen <sampo.savolainen@f5e34f95-6847-7746-d8f2-e330973f9afd>
2011-01-09 13:18:51 +0000

    Added VSTSDK environment variable for win32 builds

--- tools/win32/env.sh
+++ tools/win32/env.sh
@@ -23,6 +23,8 @@ export WINDRES=i586-mingw32msvc-windres
 
 export PATH=$YC20_TOOL_PATH/fake-utils:$PATH
 
+export VSTSDK=$YC20_TOOL_PATH/vstsdk2.4/
+
 export LDFLAGS_YC20="-mwindows "$YC20_TOOL_PATH"/gui.o "$YC20_TOOL_PATH"/ringbuffer.o"
 export LDFLAGS_CLI=$YC20_TOOL_PATH"/ringbuffer.o"
 
@@ -34,3 +36,7 @@ $WINDRES $YC20_TOOL_PATH/gui.rc -o $YC20_TOOL_PATH/gui.o
 echo compiling ringbuffer.c
 $CC $YC20_TOOL_PATH/jack-1.9.6/jack-1.9.6/common/ringbuffer.c -I$YC20_TOOL_PATH/jack-1.9.6/jack-1.9.6/windows $CFLAGS -c -o $YC20_TOOL_PATH/ringbuffer.o
 
+if [ ! -d $VSTSDK ]; then
+	echo '########## WARNING ############'
+	echo cannot build the VST. Please download the VST SDK 2.4 and unpack it so that it appears as $VSTSDK
+fi
