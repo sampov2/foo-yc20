54b8d7df75bcd11f08398364fc10e8f51d5074ad
rgareus <rgareus@f5e34f95-6847-7746-d8f2-e330973f9afd>
2011-01-26 03:23:50 +0000

    OSX/VSTi: load graphics from bundle path

--- include/foo-yc20-os.h
+++ include/foo-yc20-os.h
@@ -40,7 +40,8 @@
 #ifdef OSXLV2 // OSX LV2
 #define INSTALL_LOCATION PREFIX_STR SHARE_DIR
 #elif defined OSXVST
-#define INSTALL_LOCATION "/Users/rgareus/src/foo-yc20/trunk/" // XXX -> @loader_path/../Resources/
+const char * get_bundle_path(void);
+#define INSTALL_LOCATION std::string(get_bundle_path()) + "/" +
 #else // OSX app bundle
 #define INSTALL_LOCATION "../Resources/"
 #endif
