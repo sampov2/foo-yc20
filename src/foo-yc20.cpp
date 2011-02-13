c65452eff87ceb14879fea20e28b56c91a2cc840
sampo.savolainen <sampo.savolainen@f5e34f95-6847-7746-d8f2-e330973f9afd>
2011-02-13 12:03:31 +0000

    Fix license wording

--- src/foo-yc20.cpp
+++ src/foo-yc20.cpp
@@ -1,7 +1,7 @@
 /*
     Foo-YC20 UI utility class
 
-Copyright 2010-2011 Sampo Savolainen. All rights reserved.
+Copyright 2010-2011 Sampo Savolainen (v2@iki.fi). All rights reserved.
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
@@ -12,16 +12,16 @@ are permitted provided that the following conditions are met:
       this list of conditions and the following disclaimer in the documentation
       and/or other materials provided with the distribution.
 
-   3. Neither the name of Foo YC20 nor the names of its contributors may be 
-      used to endorse or promote products derived from this software without
-      specific prior written permission.
+   3. Neither the name Foo YC20, its author, nor the names of its contributors
+      may be used to endorse or promote products derived from this software
+      without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY SAMPO SAVOLAINEN ``AS IS'' AND ANY EXPRESS OR
 IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
 SHALL SAMPO SAVOLAINEN OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
 INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OFSUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
 PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
 LIABILITY, WHETHERIN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
